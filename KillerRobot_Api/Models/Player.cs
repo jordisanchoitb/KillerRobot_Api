70c06e6595ce637eb480aaa8352269b463ce3fff
David-Galan-Cardona <david.galan.7e7@itb.cat>
2024-05-15 13:58:02 +0200

    Made model player and madel scores is in process

---
+++ KillerRobot_Api/Models/Player.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KillerRobot_Api.Models
+{
+    public class Player
+    {
+        [Key]
+        public string name { get; set; }
+    }
+}
---
+++ KillerRobot_Api/Models/Scores.cs
@@ -0,0 +1,8 @@
+﻿namespace KillerRobot_Api.Models
+{
+    public class Scores
+    {
+        public int id { get; set; }
+        public Player player { get; set; }
+    }
+}
